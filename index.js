af0b98fdeece73e33fd0d408efe75179b5da7c0a
luisfelipegomesr <luisfelipegomesr@gmail.com>
2023-07-04 00:19:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyparser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var conString = config.urlConnection;
 var client = new Client(conString);
 client.connect(function (err) {

e26939bc0a523761a3bba87c8f6ac0608100efc3
luisfelipegomesr <luisfelipegomesr@gmail.com>
2023-07-03 23:03:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyparser.json());
-
 app.use((req, res, next) => {
-    res.header("Access-Control_Allow-Credentials", "true")
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Origin", "*"); // Defina o domínio do seu aplicativo frontend
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
